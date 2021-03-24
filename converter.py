0de10854353f3128107d3a6d743e211bc9d0b5b6
Firre <simon200021@gmail.com>
2021-03-24 10:01:22 +0100

    Added stop support and tickrate support

--- converter.py
+++ converter.py
@@ -34,6 +34,7 @@ def getSplit(measure):
 	split = len(measure)/4
 	return int(split)
 
+#Returns a template to print for ucs files
 def splitTemplate(bpm, split, delay = 0, beat = 4):
 	template = f""":BPM={bpm}
 :Delay={delay}
@@ -41,16 +42,16 @@ def splitTemplate(bpm, split, delay = 0, beat = 4):
 :Split={split}"""
 	return template
 
+#Doubles the specified measure and returns the notes
 def doubleMeasure(measureIndex, notes):
 	for noteMeasure in range(getSplit(notes[measureIndex]) * 4, 0, -1):
 		notes[measureIndex].insert(noteMeasure, "." * len(notes[0][0]))
 	notes = addHoldsMeasure(measureIndex, notes)
 	return notes
 
+#Adds holds to a specific measure
 def addHoldsMeasure(measureIndex, notes):
 	ishold = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-	#print(measureIndex)
-	#print((notes[measureIndex - 1][-1]))
 	for note in range(len(notes[measureIndex - 1][-1])):
 		curNote = notes[measureIndex - 1][-1][note]
 		if curNote == "M":
@@ -59,7 +60,6 @@ def addHoldsMeasure(measureIndex, notes):
 		if curNote == "H":
 			ishold[note] = 1
 
-	#print(ishold)
 	for beat in range(len(notes[measureIndex])):
 			for note in range(len(notes[measureIndex][beat])):
 						curNote = notes[measureIndex][beat][note]
@@ -75,6 +75,7 @@ def addHoldsMeasure(measureIndex, notes):
 							ishold[note] = 0
 	return notes
 
+#Add holds to the entire chart
 def addHolds(notes):
 	for measureIndex in range(len(notes)):
 		notes = addHoldsMeasure(measureIndex, notes)
@@ -88,11 +89,10 @@ def addHolds(notes):
 #If int then double the measure, if not int then repeat untill int or untill measure is 128
 ##
 
+#Given a bpm, doubles the measure of the bpm untill it fits
 def bpmMeasure(notes, bpm):
 	measureIndex = int(bpm[0])
-	print(bpm[0])
 	decimal = bpm[0] - int(bpm[0])
-	print(decimal)
 	notInt = 1
 	while notInt:
 		if (decimal * getSplit(notes[measureIndex])).is_integer():
@@ -104,7 +104,26 @@ def bpmMeasure(notes, bpm):
 			break
 	return notes
 
+def tickFix(notes, tickCounts):
+	tickCounter = 0
+	for tickId, tick in enumerate(tickCounts):
+		tickCounter += 1
+		print(f"{tickCounter}, {tick[1]}")
+		print(f"{tickId}, {len(tickCounts)}")
+		if tickId != len(tickCounts) - 1:
+			maxMeasure = int(tickCounts[tickId+1][0])
+		else:
+			maxMeasure = len(notes)
 
+		for measure in range(int(tick[0]), maxMeasure):
+			loop = 1
+			#print(measure)
+			while loop:
+				if getSplit(notes[measure]) < float(tick[1]):
+					doubleMeasure(measure, notes)
+				else:
+					loop = 0
+	return notes
 
 
 def main():
@@ -169,6 +188,9 @@ def main():
 		#Get tick counts
 		tickCounts = getField(indata, "#TICKCOUNTS:")
 
+		for i, tick in enumerate(tickCounts):
+			tickCounts[i][0] = float(tick[0])/4
+
 		#Get offset
 		offset = float(getField(indata, "#OFFSET:"))*1000
 
@@ -179,7 +201,12 @@ def main():
 
 		#Get stops
 		stops = getField(indata, "#STOPS:")
+		for i, stop in enumerate(stops):
+			stops[i][0] = float(stop[0])/4
+			stops[i][1] = float(stop[1])*1000
 
+
+		#Get the stepstyle
 		stepstyle = len(notes[0][0])
 		if stepstyle == 10:
 			stepstyle = "Double"
@@ -194,6 +221,12 @@ def main():
 		#Fix measures so that bpm changes can fit
 		for bpm in bpms:
 			notes = bpmMeasure(notes, bpm)
+		
+		for stop in stops:
+			notes = bpmMeasure(notes, stop)
+
+
+		notes = tickFix(notes, tickCounts)
 
 		
 		
@@ -202,6 +235,7 @@ def main():
 		actualNoteCount = 0
 		actualNoteOffset = 0
 		actualNote = 0
+		stopCounter = 0
 		splitTime = True
 
 
@@ -219,7 +253,7 @@ def main():
 
 
 			#Loop through the beats
-			for beatId, beat in enumerate(measure):
+			for beat in measure:
 
 				#Keeps track of where you are in the chart
 				actualNoteCount += 1
@@ -235,15 +269,24 @@ def main():
 					splitTime = False
 					f.write(splitTemplate(bpms[bpmCounter][1], split) + "\n")
 
+				#Adds stops
+				if stops:
+					if trueBeat == stops[stopCounter][0]:
+						print(f"STOP: {stopCounter}, {trueBeat}, {stops[stopCounter][0]}, {stops[stopCounter][1]}")
+						f.write(splitTemplate(bpms[bpmCounter][1], split, stops[stopCounter][1]) + "\n")
+						if stopCounter != len(stops)-1:
+							stopCounter += 1
+
 				#New bpm for next beat
 				if len(bpms) != bpmCounter+1:
 					if trueBeat == bpms[bpmCounter+1][0]:
-						print(f"{bpmCounter}, {trueBeat}, {bpms[bpmCounter][0]}, {bpms[bpmCounter][1]}")
+						print(f"BPM: {bpmCounter}, {trueBeat}, {bpms[bpmCounter+1][0]}, {bpms[bpmCounter+1][1]}")
 						splitTime = True
 						bpmCounter += 1
 
 				f.write(beat + "\n")
 
+				
 		f.close()
 	
 	
